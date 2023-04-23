80650f2f86b713e8b206842bf80754e1cfe52047
marcinpytlik <77489303+marcinpytlik@users.noreply.github.com>
2023-04-23 18:31:05 +0200

    ok

---
+++ KursJęzyka/przydatnepolecenia.ps1
@@ -0,0 +1,10 @@
+# Przydatne polecenia oraz moduły do pracy
+Set-StrictMode -Version Latest
+Get-Variable
+Select-Object -InputObject $color -Property *
+Get-Member -InputObject $color
+$zmienna="ala"
+[int] $zmienna # gwarancja typu danych
+strona 65
+https://geekshangout.com/install-all-rsat-tools-via-powershell/?utm_content=cmp-true
+https://www.darkoperator.com/blog/2013/3/5/powershell-basics-execution-policy-part-1.html
